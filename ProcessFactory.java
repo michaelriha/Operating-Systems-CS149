712549306eb2f1c74ed1472e1b298b1b58b99ccb
ManZoor <manzoor_81@hotmail.com>
2013-06-18 10:15:34 -0700

    added comments to ProcessFactory.java

--- ProcessFactory.java
+++ ProcessFactory.java
@@ -2,6 +2,13 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
 
+/*
+ * ProcessFactory generates processes
+ * @author Manzoor Ahmed, Igor Sorokin
+ * @date 06/18/13
+ * @version 1.0.0
+ *  
+ * **/
 public class ProcessFactory {
 
 	private int numbers; // numbers of process we need to generate
@@ -17,29 +24,39 @@ public class ProcessFactory {
 	 * **/
 	
 	public Queue<Process> generateProcesses(){
-		
+		//process name
 		String names ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 		//Queue to hold all newly generated process
 		Queue<Process> q = new LinkedList<Process>();
 		
 		for(int start =0; start!=numbers; start++){
 			
+			//create new process
 			Process p = new Process();
 			
+			//rand generate process arrival time
 			Random randomArrival = new Random(3);
-			randomArrival.setSeed(0);
-			Random randomExpectedTime = new Random(1);
-			randomExpectedTime.setSeed(0);
-			Random randomPriority = new Random(4);
-			randomPriority.setSeed(0);
+			//randomArrival.setSeed(0);
 			
+			//rand generate  
+			Random randomExpectedTime = new Random(1);
+			//randomExpectedTime.setSeed(0);
+			
+			Random randomPriority = new Random(4);
+			//randomPriority.setSeed(0);
+			
+			//set process information, ie name, time, priority
 			p.arrivalTime = randomArrival.nextFloat(); 
 			p.expectedRunTime=randomExpectedTime.nextInt();
 			p.priority = randomPriority.nextInt();
-			p.name = names.charAt(start) + Integer.toString(start);
 			
+			//TODO need to fix process name 
+			p.name = names.charAt(start) + Integer.toString(start);
+			//add newly fresh generated process to the queue
 			q.add(new Process());
 		}
+		
+		//get the entire queue
 		return q;
 	}
 }
