c903d18e0afb40f0f39d00104904bc99b48cab04
ManZoor <manzoor_81@hotmail.com>
2013-06-18 10:20:02 -0700

    added comment to Main.java

--- Main.java
+++ Main.java
@@ -4,14 +4,22 @@ import java.util.Queue;
 import java.util.Random;
 import java.util.Iterator;
 
+/*
+ * 
+ * **/
+
 public class Main{
 
 	public static void main(String[] args) {
 		
+		//generate 10 process for testing
 		ProcessFactory factory = new ProcessFactory(10);
+		//to hold returned processes
 		Queue<Process> q = new LinkedList<Process>();
+		//queue full of process
 		q = factory.generateProcesses();
 		
+		//print all process using iterrator
 		Iterator<Process> iter= q.iterator();
 		
 		
