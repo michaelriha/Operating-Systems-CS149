1fa6ff6da83beeb189516203359217c7b24e3bf2
ManZoor <manzoor_81@hotmail.com>
2013-06-18 23:26:30 -0700

    I am not generating Randoms correctly?

--- ProcessFactory.java
+++ ProcessFactory.java
@@ -38,33 +38,37 @@ public class ProcessFactory {
 		//Queue to hold all newly generated process
 		Queue<Process> q = new LinkedList<Process>();
 		
+
+		//rand generate process arrival time
+		Random randomArrival = new Random(100);
+		//randomArrival.setSeed(0);
+		
+		//rand generate  
+		Random randomExpectedTime = new Random(10);
+		//randomExpectedTime.setSeed(0);
+		
+		Random randomPriority = new Random(4);
+		
 		//how many processes do we want to create
 		for(int start =0; start!=numbers; start++){
 			
 			//create new process
 			Process p = new Process();
 			
-			//rand generate process arrival time
-			Random randomArrival = new Random(3);
-			//randomArrival.setSeed(0);
-			
-			//rand generate  
-			Random randomExpectedTime = new Random(1);
-			//randomExpectedTime.setSeed(0);
-			
-			Random randomPriority = new Random(4);
 			//randomPriority.setSeed(0);
 			
 			//set process information, ie name, time, priority
-			p.arrivalTime = randomArrival.nextFloat(); 
-			p.expectedRunTime=randomExpectedTime.nextInt();
-			p.priority = randomPriority.nextInt();
+			p.arrivalTime = randomArrival.nextFloat()+1; 
+			p.expectedRunTime=randomExpectedTime.nextInt(10)+1;
+			p.priority = randomPriority.nextInt(4)+1;
 			
 			//TODO need to fix process name 
 			p.name = names.charAt(start) + Integer.toString(start);
 			
 			//add newly fresh generated process to the queue
 			q.add(new Process());
+			
+			System.out.println(randomArrival.nextFloat());
 		}
 		
 		//get the entire queue filled with fresh backed processes!
