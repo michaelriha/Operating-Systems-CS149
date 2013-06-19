dd8403d503cfac7b9fd51083da22ee47a9d6e26d
Michael <Michael@Michael-PC>
2013-06-19 05:48:23 -0700

    real actual genuine final version

--- FirstComeFirstServed.java
+++ FirstComeFirstServed.java
@@ -15,7 +15,7 @@ public class FirstComeFirstServed extends Scheduler
     @Override
     public Queue<Process> schedule(PriorityQueue<Process> q) 
     {
-        int quanta = 0; // the current time slice
+        int startTime = 0; // the current time slice
         int queueSize = q.size();
         int finishTime = 0;
         Process p;
@@ -28,25 +28,26 @@ public class FirstComeFirstServed extends Scheduler
             p = q.poll(); // MUST BE POLLED SEPARATELY for PriorityQueue to update
                           // for (Process p : q) will give the wrong order!
             
-            // If not waiting for another process
-            if (quanta < p.arrivalTime)
-                quanta = (int) Math.ceil(p.arrivalTime);
-            else
-                quanta = finishTime;
+            startTime = Math.max((int) Math.ceil(p.arrivalTime), finishTime);            
             
             // Record the statistics for this process
-            stats.addWaitTime(quanta - p.arrivalTime);
-            stats.addTurnaroundTime(quanta - p.arrivalTime + p.burstTime);
-            stats.addResponseTime(quanta - p.arrivalTime + p.burstTime);
+            stats.addWaitTime(startTime - p.arrivalTime);
+            stats.addTurnaroundTime(startTime - p.arrivalTime + p.burstTime);
+            stats.addResponseTime(startTime - p.arrivalTime + p.burstTime);
             stats.addProcess();
-            finishTime = quanta + p.burstTime;
+            finishTime = startTime + p.burstTime;
             
+            // Don't start any processes after 100 time slices
+            if (startTime > 100)
+                break;
+
             // Create a new process with the calculated start time and add to a new queue
             scheduled = new Process();
             scheduled.burstTime = p.burstTime;
-            scheduled.startTime = quanta;
+            scheduled.startTime = startTime;
             scheduled.name = p.name;
             scheduledQueue.add(scheduled);
+            
         }
         stats.addQuanta(finishTime); // Add the total quanta to finish all jobs
         return scheduledQueue;
