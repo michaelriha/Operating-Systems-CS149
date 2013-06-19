0a82209faeb62d1a0058ce48464a3891b78aeeda
Michael <Michael@Michael-PC>
2013-06-19 02:07:39 -0700

    Adopted PriorityQueue to sort based on arrival time

--- ProcessFactory.java
+++ ProcessFactory.java
@@ -1,4 +1,4 @@
-import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Random;
 
@@ -8,70 +8,58 @@ import java.util.Random;
  * 
  * @author Manzoor Ahmed
  * @author Igor Sorokin
- * @author 
+ * @author Michael Riha
  * 
  * @date 06/18/13
  * @version 1.0.0
  *  
  * ******************************************************************************/
 
-public class ProcessFactory {
+public class ProcessFactory
+{
+    private int numbers; // numbers of process we need to generate
 
-	private int numbers; // numbers of process we need to generate
+    public ProcessFactory(int numbers)
+    {
+        this.numbers = numbers;
+    }
 
-	public ProcessFactory(int numbers){
-		this.numbers = numbers;
-	}
-	
-	/*
-	 * generateProcesses creates process with name, arrival time,
-	 * expected run time, priority and shoves them in queue.
-	 * @return q, queue full of processes
-	 * **/
-	
-	public Queue<Process> generateProcesses(){
-		//process name
-		
-		//TODO-- Igor, we need to change this to random char instead.
-		String names ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-		
-		//Queue to hold all newly generated process
-		Queue<Process> q = new LinkedList<Process>();
-		
+    /*
+     * generateProcesses creates process with name, arrival time,
+     * expected run time, priority and shoves them in queue.
+     * @return q, queue full of processes
+     * **/
 
-		//rand generate process arrival time
-		Random randomArrival = new Random(100);
-		//randomArrival.setSeed(0);
-		
-		//rand generate  
-		Random randomExpectedTime = new Random(10);
-		//randomExpectedTime.setSeed(0);
-		
-		Random randomPriority = new Random(4);
-		
-		//how many processes do we want to create
-		for(int start =0; start!=numbers; start++){
-			
-			//create new process
-			Process p = new Process();
-			
-			//randomPriority.setSeed(0);
-			
-			//set process information, ie name, time, priority
-			p.arrivalTime = randomArrival.nextFloat()+1; 
-			p.expectedRunTime=randomExpectedTime.nextInt(10)+1;
-			p.priority = randomPriority.nextInt(4)+1;
-			
-			//TODO need to fix process name 
-			p.name = names.charAt(start) + Integer.toString(start);
-			
-			//add newly fresh generated process to the queue
-			q.add(new Process());
-			
-			System.out.println(randomArrival.nextFloat());
-		}
-		
-		//get the entire queue filled with fresh backed processes!
-		return q;
-	}
+    public Queue<Process> generateProcesses()
+    {
+        //TODO-- Igor, we need to change this to random char instead.
+        // ^^ The process names do not have to be random. - Michael
+        String names ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        // Use a priority queue to order processes by arrival time (IMPORTANT!)
+        Queue<Process> q = new PriorityQueue<>();
+
+        // get random arrival, expected time, and priority
+        Random randomArrival = new Random(100);
+        Random randomExpectedTime = new Random(10);
+        Random randomPriority = new Random(4);
+        //randomArrival.setSeed(0);
+        //randomExpectedTime.setSeed(0);		
+
+        for(int start = 0; start != numbers; ++start)
+        {		
+            Process p = new Process();
+
+            p.arrivalTime = randomArrival.nextFloat()+1; 
+            p.burstTime = randomExpectedTime.nextInt(10)+1;
+            p.priority = randomPriority.nextInt(4)+1;
+
+            //TODO need to fix process name -- Should just be a letter 
+            p.name = names.charAt(start);
+            q.add(p);
+
+            System.out.println(randomArrival.nextFloat());
+        }
+        return q;
+    }
 }
