ecab3d71ae59ee46d07498288b273528948a60ee
Michael <Michael@Michael-PC>
2013-06-19 04:48:47 -0700

    changed schedule(Queue) to schedule(PriorityQueue)

--- PreemptiveHighestPriorityFirst.java
+++ PreemptiveHighestPriorityFirst.java
@@ -1,10 +1,11 @@
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class PreemptiveHighestPriorityFirst extends Scheduler 
 {
 
     @Override
-    public Queue<Process> schedule(Queue<Process> q) 
+    public Queue<Process> schedule(PriorityQueue<Process> q) 
     {
         throw new UnsupportedOperationException("Not supported yet.");
     }
--- RoundRobin.java
+++ RoundRobin.java
@@ -1,9 +1,10 @@
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class RoundRobin extends Scheduler 
 {
     @Override
-    public Queue<Process> schedule(Queue<Process> q) {
+    public Queue<Process> schedule(PriorityQueue<Process> q) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 }
